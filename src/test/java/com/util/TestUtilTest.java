df676a50473354dba97ecc678f05190c909c1588
YangHJ2415 <guswns2415@naver.com>
2025-06-02 11:59:57 +0900

    6강

--- src/test/java/com/util/TestUtilTest.java
+++ src/test/java/com/util/TestUtilTest.java
@@ -4,14 +4,15 @@ import com.TestUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestUtilTest {
+
     @Test
     @DisplayName("TestUtil.genScanner()")
-
     public void t1() {
         Scanner scanner = TestUtil.genScanner("""
                등록
@@ -27,4 +28,20 @@ public class TestUtilTest {
         assertThat(content).isEqualTo("나의 죽음을 적들에게 알리지 말라!");
         assertThat(author).isEqualTo("이순신");
     }
+
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray()")
+    public void t2() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray();
+
+        assertThat(out).isEqualTo("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+        System.out.println("이제는 화면에 출력됩니다.");
+
+    }
+
 }
