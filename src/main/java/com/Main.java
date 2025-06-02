1f484a49beb5a61ea7ff687bc11d140ad0ae9b9b
YangHJ2415 <guswns2415@naver.com>
2025-06-02 11:46:52 +0900

    4강, 5강 TestUtil을 TDD방식으로 개발

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,12 +1,15 @@
 package com;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        lab1();
+//        lab1();
+//        lab2();
     }
 
     private static void lab1() {
@@ -22,5 +25,23 @@ public class Main {
 
         System.out.println("cmd : " + cmd);
         System.out.println("content : " + content);
+    }
+
+    private static void lab2() {
+        PrintStream ORIGINAL_OUT = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream, true);
+        System.setOut(printStream);
+
+        System.out.println("Hello");
+        String str1 = byteArrayOutputStream.toString();
+        System.out.println("Nice");
+        String str2 = byteArrayOutputStream.toString();
+
+        // Reset the System.out to original
+        System.setOut(ORIGINAL_OUT);
+        printStream.close();
+        System.out.println(str1 + str2 );
     }
 }
---
+++ src/test/java/com/AppTest.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    @DisplayName("WiseSayings Test")
+    void t1(){
+
+    }
+}
